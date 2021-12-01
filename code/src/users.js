91884a0ac15ea8c9d9ea1d3bd909e95f01969137
AGLanfranchi <adam.lanfranchi@gmail.com>
2021-12-01 13:47:24 +0000

    Updated file

--- code/src/users.js
+++ code/src/users.js
@@ -10,7 +10,7 @@ module.exports = {
         Database.connect().then(db => {
             db.get('SELECT * FROM users WHERE username = ? AND password = ?', username, password).then(result => {
                 if (result && !result.token) {
-                    // Create an API toekn for the uer if they don't have one
+                    // Create an API toekn for the user if they don't have one
                     let token = UUID.v4();
                     db.run('UPDATE users SET token = ? WHERE id = ?', token, result.id).then(() => {
                         result.token = token
