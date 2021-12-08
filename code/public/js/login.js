d8a72e7ba26b6c71cbfbd9e5475a6bd2a6ee5d86
AGLanfranchi <adam.lanfranchi@gmail.com>
2021-12-08 15:45:38 +0000

    Update login.js
    Changed some spacing

--- code/public/js/login.js
+++ code/public/js/login.js
@@ -1,47 +1,46 @@
 // Change the type of input to password or text
 function Toggle() {
-    var temp = document.getElementById("passwordInput");
-    if (temp.type === "password") {
-        temp.type = "text";
-    } else {
-        temp.type = "password";
-    }
-   }  
+        var temp = document.getElementById("passwordInput");
+        if (temp.type === "password") {
+            temp.type = "text";
+        } else {
+            temp.type = "password";
+        }
+    }  
 
    function login(user) {
-    let loginErrorMsg = document.getElementById("loginErrorMessage")
-
-    if (user && user.token) {
-        window.sessionStorage.setItem('token', user.token) // Save the user's API token
-        window.sessionStorage.setItem('username', user.username)
-        loginErrorMsg.classList.add('hidden')
-        window.location = 'http://localhost:3000/index.html'
-    } else {
-        loginErrorMsg.classList.remove('hidden')
+        let loginErrorMsg = document.getElementById("loginErrorMessage")
+        if (user && user.token) {
+            window.sessionStorage.setItem('token', user.token) // Save the user's API token
+            window.sessionStorage.setItem('username', user.username)
+            loginErrorMsg.classList.add('hidden')
+            window.location = 'http://localhost:3000/index.html'
+        } else {
+            loginErrorMsg.classList.remove('hidden')
+        }
     }
-   }
 
 // Use fetch to call the API
 
 // function callApi(url, data) look at moving this later to another file
 
 function callApi(data) {
-    let url="/api/login"
-    let options ={
-     method: "POST",
-     headers: {
-         "Content-Type" : "application/json"
-     },
-     body: JSON.stringify(data)
-  }
+        let url="/api/login"
+        let options ={
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify(data)
+    }
 
-    return fetch(url, options).then(response => response.json());
-}
+        return fetch(url, options).then(response => response.json());
+    }
 
-    let formInput = document.getElementById("loginForm");
-    
-                    // Add submit event listener.
-	                formInput.addEventListener("submit", (event) => { 
+        let formInput = document.getElementById("loginForm");
+        
+                        // Add submit event listener.
+                        formInput.addEventListener("submit", (event) => { 
 	        	         event.preventDefault();
 
                          // Capture the input values using FormData
