1ba985a819ae7c059821dbc426707252fac4596a
AGLanfranchi <adam.lanfranchi@gmail.com>
2021-12-08 15:20:54 +0000

    Update post.js
    Added query to  order database

--- code/src/posts.js
+++ code/src/posts.js
@@ -4,15 +4,24 @@ const UUID = require('uuid')
 module.exports = {
 
     insertPost(title, body, callback) {
-    Database.connect().then(db => {
-        
-        db.run('INSERT INTO posts("title", "body") VALUES(?, ?)', title, body).then(result => {
-
-            callback()
+        Database.connect().then(db =>  {
+            db.run('INSERT INTO posts("title", "body") VALUES(?, ?)', title, body).then(result => {
+                callback()
+            })
+            .catch(err => {
+                console.log('post failed with error:' + err)
+            })
         })
-         .catch(err => {
-             console.log('post failed with error:' + err)
-             })
+    },
+
+    getPosts(offset, limit, callback) {
+        Database.connect().then(db =>  {
+            db.all('SELECT * FROM posts ORDER BY id DESC LIMIT ? OFFSET ?', limit, offset).then(result => {
+                callback(result)
+            })
+            .catch(err => {
+                console.log('post failed with error:' + err)
+            })
         })
     }
 }
