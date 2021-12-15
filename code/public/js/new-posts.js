032ae841cac2d6d95273f3287a20ff3d36fccbd9
AGLanfranchi <adam.lanfranchi@gmail.com>
2021-12-15 11:54:40 +0000

    Update new-post.js
    Content-Type and application/jason removed.
    Not sure its working

--- code/public/js/new-posts.js
+++ code/public/js/new-posts.js
@@ -4,10 +4,11 @@
     let url="/api/post"
     let options ={
         method: "POST",
-             headers: {
-                "Content-Type" : "application/json"
-        },
-                body: JSON.stringify(data)
+        //      headers: {
+        //         "Content-Type" : "application/json"
+        // },
+                body: new FormData(form)
+                // JSON.stringify(data)
             }
 
     return fetch(url, options).then(response => response.json());
