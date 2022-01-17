c3cdf374702aab71cb186c89cd7f484a8a17e739
AGLanfranchi <adam.lanfranchi@gmail.com>
2022-01-17 15:14:03 +0000

    Update posts.js
    Did some code with Gary. Load more button working but Gary will look at this as it's a problem everyone will be having

--- code/src/posts.js
+++ code/src/posts.js
@@ -5,6 +5,9 @@ module.exports = {
 
     insertPost(title, body, user_id, image_id, callback) {
         Database.connect().then(db =>  {
+            // XSS code. Need to install html-escaper npm
+            // title = htmlEscaper.escape(title)
+            // body = htmlEscaper.escape(body)
             db.run('INSERT INTO posts("title", "body", "user_id", "image_id") VALUES(?, ?, ?, ?)', title, body, user_id, image_id).then(result => {
                 callback()
             })
@@ -28,6 +31,8 @@ module.exports = {
     getPosts(offset, limit, callback) {
         Database.connect().then(db =>  {
             db.all('SELECT * FROM posts JOIN images ON posts.image_id = images.id ORDER BY id DESC LIMIT ? OFFSET ?', limit, offset).then(result => {
+                // This one makes load more button work Gary going to look at the problem
+                // db.all('SELECT * FROM posts ORDER BY id DESC LIMIT ? OFFSET ?', limit, offset).then(result => {
                 callback(result)
             })
             .catch(err => {
