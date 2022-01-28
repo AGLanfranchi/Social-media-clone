9159dd6c8aa4ef0878f16636386c397a694c8bb4
AGLanfranchi <adam.lanfranchi@gmail.com>
2022-01-28 13:13:01 +0000

    Updated post.js
    Changed the SQL code to fix the bug where it wasn't getting the posts id

--- code/src/posts.js
+++ code/src/posts.js
@@ -5,38 +5,38 @@ const htmlEscaper = require('html-escaper')
 module.exports = {
 
     insertPost(title, body, user_id, image_id, callback) {
-        Database.connect().then(db =>  {
+        Database.connect().then(db => {
             // XSS code
             // title = htmlEscaper.escape(title)
             // body = htmlEscaper.escape(body)
             db.run('INSERT INTO posts("title", "body", "user_id", "image_id") VALUES(?, ?, ?, ?)', title, body, user_id, image_id).then(result => {
                 callback()
             })
-            .catch(err => {
-                console.log('Post failed with error:' + err)
-            })
+                .catch(err => {
+                    console.log('Post failed with error:' + err)
+                })
         })
     },
 
     imageUpload(filepath, callback) {
-        Database.connect().then(db =>  {
+        Database.connect().then(db => {
             db.run('INSERT INTO images("filepath") VALUES(?)', filepath).then(result => {
                 callback(result)
             })
-            .catch(err => {
-                console.log('Image upload failed with error:' + err)
-            })
+                .catch(err => {
+                    console.log('Image upload failed with error:' + err)
+                })
         })
     },
 
     getPosts(offset, limit, callback) {
-        Database.connect().then(db =>  {
-            db.all('SELECT * FROM posts LEFT JOIN images ON posts.image_id = images.id ORDER BY id DESC LIMIT ? OFFSET ?', limit, offset).then(result => { 
+        Database.connect().then(db => {
+            db.all('SELECT posts.id, posts.title, posts.body, posts.user_id, images.id AS image_id, images.filepath FROM posts LEFT JOIN images ON posts.image_id = images.id ORDER BY posts.id DESC LIMIT ? OFFSET ?', limit, offset).then(result => {
                 callback(result)
             })
-            .catch(err => {
-                console.log('Get posts failed with error:' + err)
-            })
+                .catch(err => {
+                    console.log('Get posts failed with error:' + err)
+                })
         })
-    } 
+    }
 }
