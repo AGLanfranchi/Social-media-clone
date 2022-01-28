fc6b70f1d19040ee419895094594a11b8593cd0a
AGLanfranchi <adam.lanfranchi@gmail.com>
2022-01-28 13:44:42 +0000

    Update index.js
    Got home page timeline working again after bug

--- code/index.js
+++ code/index.js
@@ -121,7 +121,7 @@ app.post('/api/comment', function (req, res) {
   console.log(req.body)
   let apiToken = req.get('X-API-Token');
   Users.findByToken(apiToken, user => {
-        Comments.insertComment(req.body.body, post_id, user_id, result =>{
+        Comments.insertComment(req.body.body, req.body.post_id, user_id, result =>{
           console.log(result)
           res.status(200).json(result);
         })
